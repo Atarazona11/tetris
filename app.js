387698542507db5928d6d05dad3615bdbdf749a0
atarazon@terpmail.umd.edu <atarazona1997@gmail.com>
2022-11-30 17:08:07 -0500

    Tetris example

--- app.js
+++ app.js
@@ -1,5 +1,49 @@
 document.addEventListener('DOMContentLoader', () => {
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const ScoreDisplay = document.querySelector('#score')
+    const StartBtn = document.querySelector('#start-button')
+    const width = 10
 
+    //The Tetrominoes
+    const lTetromino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width+2],
+        [width, width*2, width*2+1, width*2+2]
+    ]
 
-    
+    const zTetramino = [
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1],
+        [0, width, width+1, width+2+1],
+        [width+1, width+2, width*2, width*2+1]
+    ]
+
+    const tTetramino = [
+        [1, width, width+1, width+2],
+        [1, width+1, width+2, width*2+1],
+        [width, width+1, width+2, width*2+1],
+        [1, width, width+1, width*2+1]
+    ]
+
+    const oTetramino = [
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1]
+    ]
+
+    const iTetramino = [
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3],
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3]
+    ]
+
+    const theTetrominoes = [lTetromino, zTetramino, tTetramino, oTetramino, iTetramino]
+
+    let currentPosition = 4
+    let current = theTetrominoes[0][0]
+
 })
---
+++ style.js
@@ -0,0 +1,9 @@
+
+const name = ['Ania', 'Kubow']
+
+function showAlert(firstName, lastName) {
+    // your code block goes here
+    alert(firstName + ' ' + lastName + ', you have been alerted!')
+}
+
+showAlert();
